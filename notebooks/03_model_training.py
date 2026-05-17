62a3526910bc6bf47df0d4f8c9a1e0dfeb60608d
CyizaLandry5 <52289151+rwema3@users.noreply.github.com>
2026-05-17 06:06:30 +0200

    model trng

---
+++ notebooks/03_model_training.py
@@ -0,0 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
---
+++ notebooks/04_model_evaluation.py
@@ -0,0 +1,2 @@
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
